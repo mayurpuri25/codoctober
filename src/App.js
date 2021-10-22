309e6d450541ff1bb03a9219a2b0696b34898abf
mayurpuri25 <79853437+mayurpuri25@users.noreply.github.com>
2021-10-22 21:29:38 +0530

    login page imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Auth from './pages/Auth/Auth';
 
 function App() {
   return (
-    <div>
-      <h1>hello world!!!! Welcome Manik</h1>
-    </div>
-  )
+    <>
+      <Auth />
+    </>
+  );
 }
 
-export default App
+export default App;
