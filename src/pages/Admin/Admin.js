b1f7c7bcc0791a05a22ef68c79aa8bc05faa37b3
Ajit-91 <ajitkumar12105@gmail.com>
2021-10-23 19:16:16 +0530

    added notes page

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -4,6 +4,7 @@ import { Route, Redirect } from 'react-router';
 import NavBar from '../../components/Navbar';
 import Timeline from '../Timeline';
 import Dashboard from '../Dashboard';
+import Notes from "../Note/Notes";
 
 const  Admin = ()=> {
     return (
@@ -12,7 +13,8 @@ const  Admin = ()=> {
             <Switch>
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/webDevelopment" component={Timeline} />
-              
+                <Route exact path="/notes" component={Notes} />
+
                 <Redirect from="*" to="/" />
             </Switch>
         </>
