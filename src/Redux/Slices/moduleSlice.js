b3aef78cd6f29acda8554d140eba24e9269366dc
Ajit-91 <ajitkumar12105@gmail.com>
2021-10-24 19:34:42 +0530

    modified module slice

--- src/Redux/Slices/moduleSlice.js
+++ src/Redux/Slices/moduleSlice.js
@@ -1,19 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    modules : []
+    modules : {
+        firstLevel : {
+            Modules : [],
+            secondLevel : {
+                SubModules : [],
+                thireLevel : {
+                    Resources : []
+                }
+            }
+        }
+    }
 }
 
 const moduleSlice = createSlice({
     name: "module",
     initialState,
     reducers: {
-        addModule : (state, action)=>{
-            state.modules = [...state.modules, action.payload]
+        addModule : (state, action) =>{
+            state.modules.firstLevel.Modules = [...state.modules.firstLevel.Modules, action.payload];
+        },
+        addSubModule : (state, action) =>{
+            const submodule = state.modules.firstLevel.secondLevel.SubModules;
+            submodule = [...submodule, action.payload];
+        },
+        addResources : (state, action) =>{
+            const resources = state.modules.firstLevel.secondLevel.thirdLevel.Resources;
+            resources = [...resources, action.payload];
         }
     }
 });
 
 export const { addModule } = moduleSlice.actions
-export const selectModule = (state)=>state.module.modules
-export default moduleSlice.reducer
+export const selectModule = (state)=>state.module.modules.firstLevel.Modules;
+export const selectSubModule = (state)=>state.module.modules.firstLevel.secondLevel.SubModules;
+export const selectResources = (state)=>state.module.modules.firstLevel.secondLevel.thirdLevel.Resources;
+export default moduleSlice.reducer;
